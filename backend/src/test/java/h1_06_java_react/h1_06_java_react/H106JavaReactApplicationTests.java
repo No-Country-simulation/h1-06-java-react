4209701af171befccd3abd5b47e03eaba9b49b03
GuillermoDivan <guillermodivan@hotmail.com>
2024-07-02 19:08:00 -0300

    starting project

---
+++ backend/src/test/java/h1_06_java_react/h1_06_java_react/H106JavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package h1_06_java_react.h1_06_java_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H106JavaReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
