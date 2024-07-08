87a9ad2fa180e58944fa5fdd05dbe7a68df84095
emanuelpps <emanuelpages.ps@gmail.com>
2024-07-08 11:23:58 -0300

    fix: added redirect to welcome modal buttons

--- frontend/src/pages/welcome/components/ButtonContainer.jsx
+++ frontend/src/pages/welcome/components/ButtonContainer.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
 import Buttons from "../../../components/Buttons";
 
 function ButtonContainer() {
   return (
     <div id="button-container">
       <div id="welcome-buttons" className="flex-column-center gapButtons">
-        <Buttons label="Iniciar Sesión" variant="primary" />
-        <Buttons label="Regístrese" variant="secondary" />
+        <Link to={"/login"}>
+          <Buttons label="Iniciar Sesión" variant="primary" value="login" />
+        </Link>
+        <Link to={"/signup"}>
+          <Buttons label="Regístrese" variant="secondary" />
+        </Link>
       </div>
     </div>
   );
