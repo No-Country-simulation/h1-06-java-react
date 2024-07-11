d999564969cf43a029d17ca1c44a3e831532fbea
emanuelpps <emanuelpages.ps@gmail.com>
2024-07-11 13:49:46 -0300

    feat: added basic structure home page patient

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import homeImage from "../../../public/assets/icons/house.svg";
 import saludImage from "../../../public/assets/icons/salud.svg";
 import calendarImage from "../../../public/assets/icons/calendar.svg";
-import profileImage from "../../../public/assets/images/profile.png"
+import profileImage from "../../../public/assets/images/profile.png";
 import "./NavBar.css";
 function NavBar() {
   return (
     <div id="navBar">
-      <div id="navBar-container">
-        <Link id="navBar-inicio" className="flex-column-center">
+      <div id="navBar-container" className="navBarContainerinterFont">
+        <Link to={"/"} id="navBar-inicio" className="flex-column-center">
           <img src={homeImage} />
           Inicio
         </Link>
@@ -16,11 +16,19 @@ function NavBar() {
           <img src={saludImage} />
           Salud
         </Link>
-        <Link id="navBar-calendar" className="flex-column-center">
+        <Link
+          to={"/calendar"}
+          id="navBar-calendar"
+          className="flex-column-center"
+        >
           <img src={calendarImage} />
           Cita
         </Link>
-        <Link id="navBar-profile" className="flex-column-center">
+        <Link
+          to={"/profile"}
+          id="navBar-profile"
+          className="flex-column-center"
+        >
           <img src={profileImage} className="profile-profile-image" />
           Perfil
         </Link>
--- frontend/src/pages/patient/home/PatientHome.jsx
+++ frontend/src/pages/patient/home/PatientHome.jsx
@@ -1,9 +1,12 @@
+import "./PatientHome.css";
+import PatientMenu from "./components/PatientMenu";
 import ProfileTitle from "./components/ProfileTitle";
 
 function PatientHome() {
   return (
-    <div>
+    <div id="patient-home">
       <ProfileTitle />
+      <PatientMenu />
     </div>
   );
 }
