c62b00ca10c118a78f4d2e43e2bf5b6ab03ed329
Sol <solabigail00@hotmail.com>
2024-07-14 18:21:48 -0300

    Add Doctor Navbar

---
+++ frontend/src/pages/doctor/home/HomeDoctor.jsx
@@ -0,0 +1,9 @@
+function homeDoctor() {
+  return (
+    <div>
+      <h2>homeDoctor</h2>
+    </div>
+  )
+}
+
+export default homeDoctor
---
+++ frontend/src/pages/doctor/perfil/Perfil.jsx
@@ -0,0 +1,9 @@
+function Perfil() {
+  return (
+    <div>
+      <h2>Perfil</h2>
+    </div>
+  )
+}
+
+export default Perfil
