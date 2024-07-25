8267c4cf2027a682a1f91ec02633b57d77c9b5a5
emanuelpps <emanuelpages.ps@gmail.com>
2024-07-25 19:41:46 -0300

    styles corrections and active navbar color

--- frontend/src/pages/patient/home/components/ProfileTitle.jsx
+++ frontend/src/pages/patient/home/components/ProfileTitle.jsx
@@ -1,12 +1,19 @@
 import profileImage from "../../../../../public/assets/images/profile.png";
+import { useUserLogin } from "../../../../store/UserLogin";
+import checkLocalTime from "../../../../hooks/checkLocalTime";
 function ProfileTitle() {
+  const { user } = useUserLogin();
   return (
     <div id="profile-title">
       <div id="profile-title-container">
         <div id="profile-image">
           <img src={profileImage} />
         </div>
-        <div id="profile-title">Hola {"user"} Buenos dias</div>
+        <div id="profile-title">
+          <span className="flex-row">
+            Hola {user.name} {checkLocalTime()}
+          </span>
+        </div>
       </div>
     </div>
   );
