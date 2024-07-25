1e2f96abb2f68ab9fdea1ed96756e34bcd0d52aa
GuillermoDivan <guillermodivan@hotmail.com>
2024-07-25 17:52:57 -0300

    Docker for Front working... ish!

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- base: "/",
- plugins: [react()],
- preview: {
-  port: 8090,
-  strictPort: true,
- },
- server: {
-  port: 8090,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:8090/",
- },
+  base: "/",
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: 8090,
+    strictPort: true,
+  },
+  server: {
+    host: true,
+    port: 8090,
+    strictPort: true,
+  },
 });
