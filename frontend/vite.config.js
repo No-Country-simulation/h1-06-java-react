9a07ae8fe2243c85148a413eeac69eb5346686dd
emanuelpps <emanuelpages.ps@gmail.com>
2024-07-25 12:49:53 -0300

    config vite correction

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,6 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://3.12.169.103:8080',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
