9fd103b7f912797556c0bb549095b56969afa595
GuillermoDivan <guillermodivan@hotmail.com>
2024-07-25 17:03:30 -0300

    dockering front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-})
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 8090,
+  strictPort: true,
+ },
+ server: {
+  port: 8090,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:8090/",
+ },
+});
